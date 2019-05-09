7f6a5cb9916c611a9bc8c79436c9e10dfdef1098
Guillaume Queste <guillaume.queste@laposte.net>
2019-05-09 10:34:00 +0200

    suppression handwriting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { ErreurComponent } from './components/erreur/erreur.component';
+import { HomeFruitsComponent } from './components/fruits/home-fruits/home-fruits.component';
+import { HomeLegumesComponent } from './components/legumes/home-legumes/home-legumes.component';
 
 const routes: Routes = [
   { path: 'accueil', component: AccueilComponent },
+  { path: 'fruits', component: HomeFruitsComponent },
+  { path: 'legumes', component: HomeLegumesComponent },
   { path: '', redirectTo: 'accueil', pathMatch: 'full' },
   { path: '**', component: ErreurComponent }
 ];
